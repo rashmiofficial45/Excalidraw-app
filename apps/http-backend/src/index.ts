fc50c52ac62bf0867c3a5554d8ecb5e9fcb5d142
Rashmi Ranjan Parija <rashmiranjanparija78898@gmail.com>
2025-03-25 21:53:10 +0530

    feat(http-backend): implement JWT authentication and add signup/signin routes

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,25 @@
-import express from "express"
-const app = express()
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { Middleware } from "./middleware";
+const app = express();
 
 app.listen(3001, () => {
-  console.log("Server is running on port 3001")
-})
+  console.log("Server is running on port 3001");
+});
+app.post("/signup", (req, res) => {
+  res.send("signup");
+});
+app.post("/signin", (req, res) => {
+  const userId = 1;
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+  res.send("signin");
+});
+app.post("/room", Middleware, (req, res) => {
+  res.send("create-room");
+});
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Response, Request, Router } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+export function Middleware(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers["authorization"] ?? "";
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
+    //@ts-ignore Note: We need to fix the below error By adding the types of the req object
+    req.userId = decoded.userId;
+    next();
+  } catch (err) {
+    res.status(401).send({ message: "Unauthorized" });
+  }
+}
