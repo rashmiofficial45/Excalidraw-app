3563a9f443c8be09dbfdbdefaee9a4c048353294
Rashmi Ranjan Parija <rashmiranjanparija78898@gmail.com>
2025-03-26 18:31:34 +0530

    imported the common folder in http-backend folder and imported the types

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,29 +1,34 @@
-import express from "express";
-import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "@repo/backend-common/config";
-import { Middleware } from "./middleware";
-const app = express();
+  import express from "express";
+  import jwt from "jsonwebtoken";
+  import { JWT_SECRET } from "@repo/backend-common/config";
+  import { Middleware } from "./middleware";
+  import {
+    createUserSchema,
+    signInSchema,
+    createRoomSchema,
+  } from "@repo/common/types";
+  const app = express();
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
-});
-app.post("/signup", (req, res) => {
-  //DB call
-  res.send("signup");
-});
-app.post("/signin", (req, res) => {
-  const userId = 1;
-  const token = jwt.sign(
-    {
-      userId,
-    },
-    JWT_SECRET
-  );
-  res.send(token);
-});
-app.post("/room", Middleware, (req, res) => {
-  //DB call
-  res.send({
-    roomId: 1212
+  app.listen(3001, () => {
+    console.log("Server is running on port 3001");
   });
-});
+  app.post("/signup", (req, res) => {
+    //DB call
+    res.send("signup");
+  });
+  app.post("/signin", (req, res) => {
+    const userId = 1;
+    const token = jwt.sign(
+      {
+        userId,
+      },
+      JWT_SECRET
+    );
+    res.send(token);
+  });
+  app.post("/room", Middleware, (req, res) => {
+    //DB call
+    res.send({
+      roomId: 1212
+    });
+  });
---
+++ packages/common/src/types.ts
@@ -0,0 +1,14 @@
+import z from "zod";
+
+export const createUserSchema = z.object({
+  username: z.string().min(3).max(20),
+  password: z.string().min(6),
+  userId: z.string(),
+});
+export const signInSchema = z.object({
+  username: z.string().min(3).max(20),
+  password: z.string().min(6),
+});
+export const createRoomSchema = z.object({
+  name: z.string().min(3).max(20),
+});
