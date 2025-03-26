251cf15fffd1fec07dba276043bc4e436fd2d473
Rashmi Ranjan Parija <rashmiranjanparija78898@gmail.com>
2025-03-27 00:00:15 +0530

    feat(http-backend): update user signup and signin endpoints, add Prisma integration, and modify middleware for JWT

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,58 +1,69 @@
-  import express from "express";
-  import jwt from "jsonwebtoken";
-  import { JWT_SECRET } from "@repo/backend-common/config";
-  import { Middleware } from "./middleware";
-  import {
-    createUserSchema,
-    signInSchema,
-    createRoomSchema,
-  } from "@repo/common/types";
-  const app = express();
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { Middleware } from "./middleware.js";
+import { prisma, Prisma } from "@repo/db/client";
+import {
+  createUserSchema,
+  signInSchema,
+  createRoomSchema,
+} from "@repo/common/types";
+const app = express();
 
-  app.post("/signup", (req, res) => {
-    const data = createUserSchema.safeParse(req.body)
-    if (!data.success) {
-      res.status(401).json({
-        message: "Invalid data",
-        errors: data.error
-      });
-      return;
-    }
-    //DB call
+app.post("/signup", async (req, res) => {
+  const parsedData = createUserSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.status(401).json({
+      message: "Invalid data",
+      errors: parsedData.error,
     });
+    return;
+  }
+  const {  name, username, password } = parsedData.data;
+  const createUser = await prisma.user.create({
+    data:{
+      name,
+      email:username,
+      password,
+    }
+  });
+  if (createUser){
+    res.send("User Created")
+  }
+});
 
-  app.post("/signin", (req, res) => {
-    const data = signInSchema.safeParse(req.body);
-    if (!data.success) {
-      res.status(401).json({
-        message: "Invalid data",
-        errors: data.error,
-      });
-      return;
-    }
-    const userId = 1;
-    const token = jwt.sign(
-      {
-        userId,
-      },
-      JWT_SECRET
-    );
-    res.send(token);
-  });
-  app.post("/room", Middleware, (req, res) => {
-    const data = createRoomSchema.safeParse(req.body);
-    if (!data.success) {
-      res.status(401).json({
-        message: "Invalid data",
-        errors: data.error,
-      });
-      return;
-    }
-    //DB call
-    res.send({
-      roomId: 1212
+app.post("/signin", (req, res) => {
+  const data = signInSchema.safeParse(req.body);
+  if (!data.success) {
+    res.status(401).json({
+      message: "Invalid data",
+      errors: data.error,
     });
+    return;
+  }
+  const userId = 1;
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+  res.send(token);
+});
+app.post("/room", Middleware, (req, res) => {
+  const data = createRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    res.status(401).json({
+      message: "Invalid data",
+      errors: data.error,
+    });
+    return;
+  }
+  //DB call
+  res.send({
+    roomId: 1212,
   });
-  app.listen(3001, () => {
-    console.log("Server is running on port 3001");
-  });
+});
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
