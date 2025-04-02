ebbae0688640fb77f66cc81dc063e2afe486973c
Rashmi Ranjan Parija <rashmiranjanparija78898@gmail.com>
2025-04-02 17:38:41 +0530

    feat(web): integrate Tailwind CSS in web and create a separate package for tailwind-config

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,21 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-
+"use client"
+import { useRouter } from "next/navigation";
+import styles from "./page.module.css"
+import { useState } from "react";
 export default function Home() {
+  const router = useRouter()
+  const [roomId, setRoomId] = useState("")
   return (
     <>
-    what's the point of staying 
+      <div className={styles.page}>
+        <div className="bg-amber-500">
+              heyyy there
+        </div>
+        <input onChange={(e) => { setRoomId(e.target.value) }} type="text" placeholder="roomId" />
+        <button onClick={()=>{
+          router.push(`room/${roomId}`)
+        }}>Join Room</button>
+      </div>
     </>
   );
 }
